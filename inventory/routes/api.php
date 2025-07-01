4d6ba7143f5b257a370fa02b8b1a793c5d74c886
Geraldine <146147105+geraldin21l@users.noreply.github.com>
2025-07-01 18:28:22 +0000

    proyecto

---
+++ inventory/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('api')->get('/test', function () {
+    return response()->json(['message' => 'API OK']);
+});
+
